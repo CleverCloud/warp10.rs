68c5eeac3442c26b09ea4a1eb56322ec891db748
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-09-15 00:30:35 +0200

    error: introduce new UrlError
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,19 @@
 extern crate hyper;
+extern crate url;
 
 use std::{error, fmt, result};
 
 #[derive(Debug)]
 pub enum Warp10Error {
-    HttpError(hyper::Error)
+    HttpError(hyper::Error),
+    UrlError(url::ParseError),
 }
 
 impl fmt::Display for Warp10Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Warp10Error::HttpError(ref err) => write!(f, "Warp10 error: {}", err)
+            Warp10Error::HttpError(ref err) => write!(f, "Warp10 HTTP error: {}", err),
+            Warp10Error::UrlError(ref err)  => write!(f, "Warp10 URL error: {}",  err),
         }
     }
 }
@@ -18,13 +21,15 @@ impl fmt::Display for Warp10Error {
 impl error::Error for Warp10Error {
     fn description(&self) -> &str {
         match *self {
-            Warp10Error::HttpError(ref err) => err.description()
+            Warp10Error::HttpError(ref err) => err.description(),
+            Warp10Error::UrlError(ref err)  => err.description(),
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
-            Warp10Error::HttpError(ref err) => Some(err)
+            Warp10Error::HttpError(ref err) => Some(err),
+            Warp10Error::UrlError(ref err)  => Some(err),
         }
     }
 }
@@ -32,6 +37,12 @@ impl error::Error for Warp10Error {
 impl From<hyper::Error> for Warp10Error {
     fn from(err: hyper::Error) -> Warp10Error {
         Warp10Error::HttpError(err)
+    }
+}
+
+impl From<url::ParseError> for Warp10Error {
+    fn from(err: url::ParseError) -> Warp10Error {
+        Warp10Error::UrlError(err)
     }
 }
 
