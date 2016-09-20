19076969a98b54848baa1e7b95712ca2259a4b89
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-09-20 10:07:55 +0200

    error: support io::Error
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,12 @@
 extern crate hyper;
 extern crate url;
 
-use std::{error, fmt, result};
+use std::{error, fmt, io, result};
 
 #[derive(Debug)]
 pub enum Error {
     HttpError(hyper::Error),
+    IoError(io::Error),
     UrlError(url::ParseError),
 }
 
@@ -13,6 +14,7 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::HttpError(ref err) => write!(f, "Warp10 HTTP error: {}", err),
+            Error::IoError(ref err)   => write!(f, "Warp10 IO error: {}", err),
             Error::UrlError(ref err)  => write!(f, "Warp10 URL error: {}",  err),
         }
     }
@@ -22,6 +24,7 @@ impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::HttpError(ref err) => err.description(),
+            Error::IoError(ref err)   => err.description(),
             Error::UrlError(ref err)  => err.description(),
         }
     }
@@ -29,6 +32,7 @@ impl error::Error for Error {
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             Error::HttpError(ref err) => Some(err),
+            Error::IoError(ref err)   => Some(err),
             Error::UrlError(ref err)  => Some(err),
         }
     }
@@ -37,6 +41,12 @@ impl error::Error for Error {
 impl From<hyper::Error> for Error {
     fn from(err: hyper::Error) -> Error {
         Error::HttpError(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
     }
 }
 
