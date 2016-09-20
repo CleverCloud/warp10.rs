1a235d8d64bb97511efc8595e5869f19b2ca630c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-09-20 10:08:02 +0200

    add Response
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,13 @@ extern crate url;
 mod client;
 mod data;
 mod error;
+mod response;
 mod token;
 mod writer;
 
 pub use client::*;
 pub use data::*;
 pub use error::*;
+pub use response::*;
 pub use token::*;
 pub use writer::*;
---
+++ src/response.rs
@@ -0,0 +1,33 @@
+use error::*;
+
+use hyper::client;
+use hyper::status::StatusCode;
+
+use std::io::Read;
+
+#[derive(Debug)]
+pub struct Response {
+    status:  StatusCode,
+    payload: String
+}
+
+impl Response {
+    pub fn new(response: &mut client::Response) -> Result<Response> {
+        let mut payload = String::new();
+
+        try!(response.read_to_string(&mut payload));
+
+        Ok(Response {
+            status:  response.status,
+            payload: payload
+        })
+    }
+
+    pub fn status(&self) -> StatusCode {
+        self.status
+    }
+
+    pub fn payload(&self) -> &str {
+        &self.payload
+    }
+}
